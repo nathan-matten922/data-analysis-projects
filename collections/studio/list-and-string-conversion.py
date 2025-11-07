ba99d7fcf08ec5de5c078280899c36fde37ed16e
Nathan-Matten922 <nmatten@gmail.com>
2025-11-06 21:59:30 -0600

    Studio Python Collections
    All 3 python studio files completed

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -6,18 +6,69 @@ proto_list4 = "Comma-spaces, might, require, typing, caution"
 strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
+connectors = [',',';'," "]
+'''for string in strings:
+    if ';' in string:
+        print(f"This list {string} has a ;")
+    elif ',' in string:
+        print(f"This list {string} has a ,")
+    else:
+        print(f"This list {string} is separated by a space")'''
+
+for string in strings:
+    for connector in connectors:
+        if connector in string:
+            if connector == " ":
+                print(f"This string {string} is separated by a space")
+            else:
+                print(f'This string {string} is separated by {connector}')
 
 
 # b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
-
+new_string = ""
+flip_string = ""
+delimeter = ""
+final_string = ""
+for string in strings:
+    if "," in string:
+        if " " not in string:
+            new_string = string.split(",")
+            flip_string = new_string[::-1]
+            final_string = ",".join(flip_string)
+            print(final_string)
+'''        else:
+            new_string = string.split(", ")
+            flip_string = new_string[::-1]
+            final_string = ",".join(flip_string)
+            print(final_string)'''
 
 
 # c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
-
+alphabet_string = ""
+for string in strings: 
+    if ";" in string:
+        alphabet_string = string.split(";")
+        alphabet_string.sort()
+        final_string = ",".join(alphabet_string)
+        print(final_string)
 
 
 # d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
 
-
+space_string = "" 
+for string in strings: 
+    if " " in string:
+        if "," not in string: 
+            space_string = string.split(' ')
+            space_string.sort(reverse= True)
+            final_string = " ".join(space_string)
+            print(final_string)
 
 # e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+comma_space = ""
+for string in strings:
+    if ", " in string:
+        comma_space = string.split(", ")
+        flip_string = comma_space[::-1]
+        final_string = ",".join(flip_string)
+        print(final_string)
--- collections/studio/multi-dimensional-lists.py
+++ collections/studio/multi-dimensional-lists.py
@@ -3,20 +3,43 @@ equipment = "space suits,jet packs,tool belts,thermal detonators"
 pets = "parrots,cats,moose,alien eggs"
 sleep_aids = "blankets,pillows,eyepatches,alarm clocks"
 
+multi = [food,equipment,pets,sleep_aids]
 # a) Use split to convert the strings into four cabinet lists. Alphabetize the contents of each cabinet.
-
-
+food = food.split(",")
+food.sort()
+equipment = equipment.split(',')
+equipment.sort()
+pets = pets.split(',')
+pets.sort
+sleep_aids = sleep_aids.split(",")
+sleep_aids.sort
 
 # b) Initialize a cargo_hold list and add the cabinet lists to it. Print cargo_hold to verify its structure.
-
+cargo_hold = [food,equipment,pets,sleep_aids]
+print(cargo_hold)
 
 
 # c) Query the user to select a cabinet (0 - 3) in the cargo_hold.
+i = input("Select the cabinet you would like to access (0-3):")
+i = int(i)
 
 
 
 # d) Use bracket notation and format to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+if i > 3 or i < 0:
+    print("INVALID ENTRY (you dunderhead)")
+else:
+    print(cargo_hold[i])
 
 
 # e) Modify the code to query the user for BOTH a cabinet in cargo_hold AND a particular item. Use the in method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
+c, value = input("Select the cabinet (1-4) and list the item you're looking for:").split()
+c = int(c) - 1
+for cabinet in cargo_hold:
+    if cabinet == cargo_hold[c]:
+        print(cabinet)
+        if value not in cabinet:
+            print(f"Cabinet {c + 1} DOES NOT contain {value}")
+        else:
+            print(f"Cabinet {c + 1} DOES contain {value}")
