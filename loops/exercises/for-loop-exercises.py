dae307a1600d9b1fc77e828b8744e972d713852a
Nathan-Matten922 <nmatten@gmail.com>
2026-01-08 20:55:50 -0600

    Update for-loop-exercises.py

--- loops/exercises/for-loop-exercises.py
+++ loops/exercises/for-loop-exercises.py
@@ -4,5 +4,17 @@
     # c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     # d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).
 
+for num in range(21):
+    print(num)
 
+for num in range(3,30):
+    if num % 2 == 1:
+        print(num)
 
+for num in range(12,-15,-1):
+    if num % 2 == 0: 
+        print(num)
+
+for num in range(50,19,-1):
+    if num % 3 == 0:
+        print(num)
